97b5bcc88ff0f08c95712caf78a7ccc3476134c2
Luis Parrado <luisprmat@gmail.com>
2024-02-20 08:33:19 -0500

    Drop select2

--- resources/views/components/select-list.blade.php
+++ resources/views/components/select-list.blade.php
@@ -8,38 +8,3 @@
         @endforeach
     </select>
 </div>
-
-@push('scripts')
-    <script>
-        document.addEventListener('livewire:init', () => {
-            const el = $('#{{ $attributes['id'] }}')
-
-            const initSelect = () => {
-                el.select2({
-                    placeholder: '{{ __('Select your option') }}',
-                    allowClear: !el.attr('required')
-                })
-            }
-
-            initSelect()
-
-            Livewire.hook('commit', ({ succeed }) => {
-                succeed(() => {
-                    queueMicrotask(() => {
-                        initSelect()
-                    })
-                })
-            })
-
-            el.on('change', () => {
-                let data = $(this)
-                // let data = $(this).select2('val')
-                console.log('data', data)
-                if (data === '') {
-                    data = null
-                }
-                @this.set('{{ $attributes['wire:model'] }}', data)
-            })
-        });
-    </script>
-@endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,9 +14,6 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Select2 styles-->
-        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-
         <!-- Scripts -->
         @vite(['resources/js/app.js'])
     </head>
@@ -38,8 +35,5 @@
                 {{ $slot }}
             </main>
         </div>
-        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-        @stack('scripts')
     </body>
 </html>
